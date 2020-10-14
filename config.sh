7db11995a3de5a39878cf3ceffe7fc76154f40fd
openluminus <>
2020-10-14 19:23:39 +0000

    new file config.sh

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Your sudoku project folder
+PROJECT_PATH="../../sudoku/"
+
+#Stores the previous working directory
+PREVIOUS_PWD=${PWD}
+
+#Your test folder
+TEST_PATH="../grid-parser"
+
+EXIT_SUCCESS=0
+EXIT_FAILURE=1
+
+VERBOSE=0
+
+FILE_OUTPUT="/dev/null"
